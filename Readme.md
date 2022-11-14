8281ca95029effe245229ad5b7176b24346e8c16
Jun99uu <igun0423@naver.com>
2022-11-15 01:11:54 +0900

    add : init commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# 🎄 Talk Me Merry Christmas 🎄
+
+> 멋쟁이 사자처럼 10기 단풍톤 10팀 프론트엔드 레포지토리 입니다.
+
+_목소리로 전하는 연말 메시지, Talk Me Merry Christmas_
---
+++ tmmc-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { RecoilRoot } from "recoil";
+
+function App() {
+  return (
+    <RecoilRoot>
+      <div></div>
+    </RecoilRoot>
+  );
+}
+
+export default App;
